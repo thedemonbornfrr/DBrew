c22af515c112e0bb0095afdcb2ad0e09693a6b2d
Revborn <thedemonbornfrr@gmail.com>
2025-11-18 21:11:14 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    appDocumentPreloading: false,
+  // THIS IS THE FIX YOU NEEDED
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": __dirname,
+    };
+    return config;
   },
 };
 
